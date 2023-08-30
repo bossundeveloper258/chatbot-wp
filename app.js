95622859559c093d41e551bfee94825dbf10d567
Edwin Soria <68561804+bossundeveloper258@users.noreply.github.com>
2023-08-30 16:42:06 -0500

    formulario

--- app.js
+++ app.js
@@ -73,9 +73,59 @@ const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
         [flowDocs, flowGracias, flowTuto, flowDiscord]
     )
 
+const flowFormulario = addKeyword(['Hola','⬅️ Volver al Inicio'])
+    .addAnswer(
+        ['Hola!','Para enviar el formulario necesito unos datos...' ,'Escriba su *Nombre*'],
+        { capture: true, buttons: [{ body: '❌ Cancelar solicitud' }] },
+
+        async (ctx, { flowDynamic, endFlow }) => {
+            if (ctx.body == '❌ Cancelar solicitud')
+             return endFlow({body: '❌ Su solicitud ha sido cancelada ❌',    // Aquí terminamos el flow si la condicion se comple
+                 buttons:[{body:'⬅️ Volver al Inicio' }]                      // Y además, añadimos un botón por si necesitas derivarlo a otro flow
+
+            
+            })
+            nombre = ctx.body
+            return flowDynamic(`Encantado *${nombre}*, continuamos...`)
+        }
+    )
+    .addAnswer(
+        ['También necesito tus dos apellidos'],
+        { capture: true, buttons: [{ body: '❌ Cancelar solicitud' }] },
+
+        async (ctx, { flowDynamic, endFlow }) => {
+            if (ctx.body == '❌ Cancelar solicitud') 
+                return endFlow({body: '❌ Su solicitud ha sido cancelada ❌',
+                    buttons:[{body:'⬅️ Volver al Inicio' }]
+
+
+        })
+        apellidos = ctx.body
+        return flowDynamic(`Perfecto *${nombre}*, por último...`)
+        }
+    )
+    .addAnswer(
+        ['Dejeme su número de teléfono y le llamaré lo antes posible.'],
+        { capture: true, buttons: [{ body: '❌ Cancelar solicitud' }] },
+
+        async (ctx, { flowDynamic, endFlow }) => {
+            if (ctx.body == '❌ Cancelar solicitud') 
+                return endFlow({body: '❌ Su solicitud ha sido cancelada ❌',
+                      buttons:[{body:'⬅️ Volver al Inicio' }]
+                })
+
+
+                telefono = ctx.body
+                // await delay(2000)
+                return flowDynamic(`Estupendo *${nombre}*! te dejo el resumen de tu formulario
+                \n- Nombre y apellidos: *${nombre} ${apellidos}*
+                \n- Telefono: *${telefono}*`)
+        }
+    )
+
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+    const adapterFlow = createFlow([flowFormulario])
 
     const adapterProvider = createProvider(MetaProvider, {
         jwtToken: 'EAACbK7Q2cwABO6ZAWDr2QTeXeVavSZCtZANvneQxytTsem95TQj2eFaZCAMaIMPhdcl8ue1eKZBuma1iBXnSh8OimEe8CMqxmDGZBSHGsPudOZAvrmWPsoMSHEj3j3pGbRMDDcXiP4eCi1Ear3OmJySiYSsKP6z0W8np1O7yhrvfBP0ZASjUlEeylJAwEscVTKx9ggx4ZBYZCtzqpKKYhz4QQZD',
