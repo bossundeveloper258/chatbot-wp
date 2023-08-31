42b4478781cb2dc4bcdff386eeccea64cede3dc5
Edwin Soria <68561804+bossundeveloper258@users.noreply.github.com>
2023-08-30 19:36:53 -0500

    optioons

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const MockAdapter = require('@bot-whatsapp/database/mock');
 
 const { getUserByDocument } = require("./services/mikrowisp.service");
 
+const { reportarPago } = require("./config/config");
+
+const  flowReportarPagoYape = require("./options/reportarPago");
+
 /**
  * Aqui declaramos los flujos hijos, los flujos se declaran de atras para adelante, es decir que si tienes un flujo de este tipo:
  *
@@ -24,64 +28,9 @@ const getUser = async (documentNumber) => {
   };
   
   //Flujos
+
   
-  const flowPagoYape = addKeyword("yape")
-    .addAnswer(
-      "Ingrese Monto de Yapeo",
-      { capture: true },
-      (ctx, { flowDynamic,fallBack }) => {
-        if (isNaN(ctx.body)) return fallBack();
-        // return flowDynamic([{body: "continua"}])
-      }
-    )
-    .addAnswer(
-      "Ingrese Fecha y Hora de Yapeo",
-      { capture: true },
-      (ctx, { fallBack }) => {
-        if (ctx.body.length == 0) return fallBack();
-      }
-    )
-    .addAnswer(
-      "Ingrese Número de Operacipon",
-      { capture: true },
-      (ctx, { fallBack }) => {
-        if (ctx.body.length == 0) return fallBack();
-      }
-    )
-    .addAnswer(
-      "Por favor Adjunte Captura del Yape",
-      { capture: true },
-      (ctx, { fallBack }) => {
-        if (ctx.body.length == 0) return fallBack();
-      }
-    )
-    .addAnswer(
-      "Se registro la información brindada",
-      null,
-      (ctx, { fallBack, endFlow }) => {
-        return endFlow({
-          body: "Muchas gracias en breve estaremos registrando su pago",
-        });
-      }
-    );
-  
-  const flowRepotarPago = addKeyword(["1"])
-    .addAnswer(
-      "Para reportar tu pago por favor indicanos el medio de pago utilizado",
-      null,
-      null,
-      []
-    )
-    .addAnswer(
-      [
-        "1- PAGO CON YAPE",
-        "2- PAGO KASNET",
-        "3- PAGOS CC. BCP",
-        "4- PAGOS CC BANCO NACION",
-      ]
-    );
-  
-  const flowPrincipal = addKeyword([
+const flowPrincipal = addKeyword([
     "Hola",
     "Buenos dias",
     "Buenas tardes",
@@ -92,7 +41,7 @@ const getUser = async (documentNumber) => {
     )
     .addAnswer(
       "Por favor ingrese el número de DNI del titular del servicio",
-      { capture: true },
+      { capture: true},
       async (ctx, { flowDynamic, fallBack }) => {
         if (ctx.body.length != 8) return fallBack();
         else {
@@ -100,7 +49,7 @@ const getUser = async (documentNumber) => {
           if (result.length > 0) {
             return await flowDynamic([
               {
-                body: `Hola ${result[0].name} por favor selecciona una de las opciones siguientes para tu atención`,
+                body: `Hola ${result[0].name}`,
               },
             ]);
           } else return fallBack();
@@ -108,19 +57,37 @@ const getUser = async (documentNumber) => {
       }
     )
     .addAnswer(
-      [
-        "1. Reportar Pago",
-        "2. Conocer el monto de mi deuda a la fecha",
-        "3. Donde puedo hacer el pago del servicio",
-      ],
-      { capture: true },
-      async (ctx, { flowDynamic, fallBack }) => {},
-      [flowRepotarPago]
+        "Por favor selecciona una de las opciones siguientes para tu atención",
+      { capture: true,
+        buttons: [
+            {body: reportarPago},
+            {body: "Conocer el monto de mi deuda a la fecha"},
+            {body: "Donde puedo hacer el pago del servicio"}
+        ]},
     );
+
+const flowOptionReportarPago = addKeyword( reportarPago )
+    .addAnswer(
+        "Para reportar tu pago por favor indicanos el medio de pago utilizado",
+        {
+            buttons: [
+                {body: "PAGO CON YAPE"},
+                {body: "PAGO KASNET"},
+                {body: "PAGOS CC. BCP"},
+                {body: "PAGOS CC BANCO NACION"}
+            ]
+        }
+    );
+
 
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal, flowPagoYape])
+    const adapterFlow = createFlow(
+        [
+            flowPrincipal, 
+            flowOptionReportarPago,
+            flowReportarPagoYape
+        ]);
 
     const adapterProvider = createProvider(MetaProvider, {
         jwtToken: 'EAACbK7Q2cwABO6ZAWDr2QTeXeVavSZCtZANvneQxytTsem95TQj2eFaZCAMaIMPhdcl8ue1eKZBuma1iBXnSh8OimEe8CMqxmDGZBSHGsPudOZAvrmWPsoMSHEj3j3pGbRMDDcXiP4eCi1Ear3OmJySiYSsKP6z0W8np1O7yhrvfBP0ZASjUlEeylJAwEscVTKx9ggx4ZBYZCtzqpKKYhz4QQZD',
---
+++ config/config.js
@@ -0,0 +1,3 @@
+const reportarPago = "Reportar Pago";
+
+module.exports = { reportarPago }
