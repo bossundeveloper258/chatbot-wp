15df3d580f558f28244e76b97a475fea9c709601
Edwin Soria <68561804+bossundeveloper258@users.noreply.github.com>
2023-09-08 18:39:40 -0500

    reportar 24

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ const flowPrincipal = addKeyword([
 
 const flowOptionReportarPago = addKeyword( reportarPago )
     .addAnswer("Selecciona el servicio", null,
-      async ( ctx, {})=>{
+      async ( ctx, {flowDynamic}) => {
         return await flowDynamic( buttonList );
       }
     )
